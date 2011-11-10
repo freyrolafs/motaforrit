d4545c9f3017afb7493c2d85e70bc81297c476e5
Freyr Ólafsson <freyr@mintan.(none)>
2011-11-10 22:56:37 +0000

    First commit

---
+++ motaskra/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Club(models.Model):
+    clubName = models.CharField(max_length=200)
+    shortName = models.CharField(max_length=200)
+    address1 = models.CharField(max_length=200)
+    address2 = models.CharField(max_length=200)
+
+
+class Event(models.Model):
+    eventName = models.CharField(max_length=200)
+    eventLocation = models.CharField(max_length=200)
+    event_date = models.DateTimeField('event first day of competition')
+    club = models.ForeignKey(Club)
+    list_display = ('eventName', 'event_date')
+
